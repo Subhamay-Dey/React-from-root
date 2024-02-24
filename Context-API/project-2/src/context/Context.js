e86b584e2895ca699bca89c3c25c1c5ea0905a13
Subhamay-Dey <ncdey1966@gmail.com>
2024-02-24 23:23:03 +0530

    made three useState hooks for the project.

---
+++ Context-API/project-2/src/context/Context.js
@@ -0,0 +1,5 @@
+import React, { createContext} from  'react';
+
+const ThemeChanger = createContext()
+
+export default ThemeChanger;
---
+++ Context-API/project-2/src/context/ThemeChangerProvider.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ThemeChangerProvider = ({children}) => {
+    const [theme, setTheme] =  React.useState('light')
+    const [themetext, setThemetext] = React.useState('black')
+    const [btnchange, setBtnchange] = React.useState('black')
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default ThemeChangerProvider
