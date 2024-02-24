058bb7fa75b887232198f0d940f04b1c94f1aeae
Subhamay-Dey <ncdey1966@gmail.com>
2024-02-24 23:50:06 +0530

    toggle function given

--- Context-API/project-2/src/components/Card.jsx
+++ Context-API/project-2/src/components/Card.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import ThemeChanger from '../context/context'
 
 const Card = () => {
+
+    const {theme} = useContext(ThemeChanger)
+
   return (
     <>
         <div className='w-full h-screen flex justify-center items-center'>
--- Context-API/project-2/src/context/ThemeChangerProvider.jsx
+++ Context-API/project-2/src/context/ThemeChangerProvider.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
+import ThemeChanger from './context'
 
 const ThemeChangerProvider = ({children}) => {
-    const [theme, setTheme] =  React.useState('light')
+    const [theme, setTheme] =  React.useState('gray')
     const [themetext, setThemetext] = React.useState('black')
     const [btnchange, setBtnchange] = React.useState('black')
-  return (
-    <>
 
-    </>
+    const changeTheme = () => {
+        if(theme === 'black'){
+            setThemetext("white")
+        }
+        else{
+            setThemetext( "black" )
+        }
+    }
+
+  return (
+    <ThemeChanger.Provider value={{theme, changeTheme, themetext, btnchange}}>
+      {children}
+    </ThemeChanger.Provider>
   )
 }
 
