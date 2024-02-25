9edb0d3817217faaa9c9a81c8126cc345d78f458
Subhamay-Dey <ncdey1966@gmail.com>
2024-02-26 02:31:50 +0530

    updateTodo function done  for updating the todo

--- Context-API/project-3/src/App.jsx
+++ Context-API/project-3/src/App.jsx
@@ -9,6 +9,21 @@ function App() {
     setTodos((prev) => [{id: Date.now(), ...todo},...prev])
   }
 
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)))
+  }
+
+  //another way to update todo
+  
+  // prev.map((eachVal) => {
+  //   if(eachVal.id === id){
+  //     todo
+  //   }
+  //   else{
+  //     prevTodo
+  //   }
+  // })
+
   return (
     <TodoProvider value={{todos, addTodo, updateTodo,deleteTodo, toggleComplete}}>
 
