3f9d82b87931037310b217e92eef0142ba6253f1
Subhamay-Dey <ncdey1966@gmail.com>
2024-02-28 23:53:41 +0530

    made a more easier method to use the context api data through the provider in different components.

--- props-project/src/contexts/product.js
+++ props-project/src/contexts/product.js
@@ -1,11 +1,14 @@
-import React, {createContext} from "react";
+import React, {createContext, useContext} from "react";
 import { useState } from "react";
 
+export const useCart = () => {
+    const cart = useContext(productContext);
+    return cart;
+};
 export const productContext= createContext();
 
 export const productProvider = (children)=> {
-    <productContext>
+    <productContext.Provider value={useProduct()}>
         {children}
-    </productContext>
+    </productContext.Provider>
 };
-
