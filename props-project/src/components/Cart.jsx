92434c80588a1dc4527d5a3bac76b097c9831ef4
Subhamay-Dey <ncdey1966@gmail.com>
2024-02-28 23:58:01 +0530

    learned to import the useContext through the provider but by a easier method which is by creating a custom hook in which my useContext api and my provider wrap is in.

--- props-project/src/components/Cart.jsx
+++ props-project/src/components/Cart.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 import Props from './Props';
+import { useCart } from '../contexts/product';
 
 const Cart = () => {
-
+  const cartItem = useCart();
   return (
     <>
       <div >
